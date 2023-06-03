684e5e3bb2854f5642a4e6e41a7d377de7eda4c4
Mohit <mohitver01@gmail.com>
2023-06-03 00:36:36 -0400

    Added unit tests for and implemented UserAdmin in core/admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for users"""
+
+    # order the users by id
+    ordering = ['id']
+    # display user email and name in the list display
+    list_display = ['email', 'name']
+
+
+# Register custom user model. Specify UserAdmin to use custom admin
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/test/test_admin.py
@@ -0,0 +1,40 @@
+"""
+Test for the Django admin modifications
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+    """Tests for django admin"""
+
+    # Create setup method to set up modules at start of different tests in this class
+    # This will be run before every single test
+    def setUp(self):
+        """Create user and client"""
+
+        # Instance of CLient class, which is a test client to get http requests
+        self.client = Client()
+        # Create superuser
+        self.admin_user = get_user_model().objects.create_superuser(email='admin@example.com', password='testpass123')
+        # Force authentication of admin_user using client
+        self.client.force_login(self.admin_user)
+
+        # Create general user in DB
+        self.user = get_user_model().objects.create_user(email='user@example.com', password='testpass123', name='Test User')
+
+    def test_users_list(self):
+        """Test that users are listed on page"""
+
+        # reverse = get url for changed list inside django admin
+        # admin:core_user_changelist = which url to pull from django admin - defined in django doc
+        url = reverse('admin:core_user_changelist')
+
+        # Make a get http request based to the url, which will be authenticated as admin_user
+        res = self.client.get(url)
+
+        # Check page contains user created and associated email
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
